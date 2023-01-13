e2d1066c352adbf909545752abc0294182e0766d
FR-User <98079782+FR-User@users.noreply.github.com>
2023-01-14 03:05:34 +0900

    [코드 오류 수정] 오타 정정

--- pluginSrc/com/codeless/projectTower/Main.java
+++ pluginSrc/com/codeless/projectTower/Main.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 
-import org.codeless.projectTower.initial.registerEvents;
+import com.codeless.projectTower.initial.registerEvents;
 
 public class Main extends JavaPlugin implements Listener{
 
